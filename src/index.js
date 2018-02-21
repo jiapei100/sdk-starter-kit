aa0644da638c406ef7632652a2eaba11f018fb2d
Guillermo Bruce <gbruce@gmail.com>
2018-02-20 21:47:59 -0800

    1st commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+
+const Config = require('../config.json');
+
+const showcaseFrame = document.getElementById('showcase');
+showcaseFrame.src = 'https://my.matterport.com/showcase-beta?m=' + Config.Sid + '&play=1';
+
+const showcase = window.SHOWCASE_SDK.connect(showcaseFrame, Config.ApiKey, '3.0')
+  .then(function(sdk) {
+    console.log('SDK Connected!');
+
+    sdk.Model.getData().then(function(modelData){
+      console.log('Model data loaded for sid:', modelData.sid);
+    });
+  })
+  .catch(function(error) {
+    console.error(error);
+  });
