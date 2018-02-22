569119351281d96047f7ad9cfdfc3033ff3797e8
Guillermo Bruce <gbruce@gmail.com>
2018-02-21 16:36:07 -0800

    add comment

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const showcase = window.SHOWCASE_SDK.connect(showcaseFrame, Config.ApiKey, '3.0'
   .then(function(sdk) {
     console.log('SDK Connected!');
 
+    // Your Matterport SDK application starts here.
     sdk.Model.getData().then(function(modelData){
       console.log('Model data loaded for sid:', modelData.sid);
     });
