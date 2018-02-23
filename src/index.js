a2d136c91fea6eea30dca0ec8445c66405b5474c
Guillermo Bruce <gbruce@gmail.com>
2018-02-23 13:27:18 -0800

    - use showcase-next - seed sample model so that something is visible on first run

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const showcaseFrame = document.getElementById('showcase');
 
 // Matterport showcase URL parameters
 // https://support.matterport.com/hc/en-us/articles/209980967-URL-Parameters
-showcaseFrame.src = 'https://my.matterport.com/showcase-beta?m=' + Config.Sid + '&play=1';
+showcaseFrame.src = 'https://showcase-next.matterport.com/show/?m=' + Config.Sid + '&play=1';
 
 window.SHOWCASE_SDK.connect(showcaseFrame, Config.ApiKey, '3.0')
   .then(function(sdk) {
