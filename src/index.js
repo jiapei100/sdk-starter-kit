af504b754885e72ca09577e06ca01f6bdee70f5f
Guillermo Bruce <gbruce@gmail.com>
2018-02-22 21:37:47 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@
 const Config = require('../config.json');
 
 const showcaseFrame = document.getElementById('showcase');
+
+// URL parameters
+// https://support.matterport.com/hc/en-us/articles/209980967-URL-Parameters
 showcaseFrame.src = 'https://my.matterport.com/showcase-beta?m=' + Config.Sid + '&play=1';
 
 window.SHOWCASE_SDK.connect(showcaseFrame, Config.ApiKey, '3.0')
