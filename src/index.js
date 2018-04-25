776a62f042fff76a0dc4e0965b801b8444b91d34
Guillermo Bruce <gbruce@gmail.com>
2018-04-24 21:48:28 -0700

    mattertag.event namespace missing after iframe load event on chrome

--- src/index.js
+++ src/index.js
@@ -6,19 +6,22 @@ const showcaseFrame = document.getElementById('showcase');
 // Matterport showcase URL parameters
 // https://support.matterport.com/hc/en-us/articles/209980967-URL-Parameters
 showcaseFrame.src = Config.Url;
+showcaseFrame.addEventListener('load', showcaseLoader, false);
 
-window.SHOWCASE_SDK.connect(showcaseFrame, Config.ApiKey, '3.0')
-  .then(function(sdk) {
-    console.log('SDK Connected!');
+function showcaseLoader() {
+  window.SHOWCASE_SDK.connect(showcaseFrame, Config.ApiKey, '3.0')
+    .then(function(sdk) {
+      console.log('SDK Connected!');
 
-    // Your Matterport SDK application starts here.
-    sdk.Model.getData().then(function(modelData){
-      console.log('Model data loaded for sid:', modelData.sid);
+      // Your Matterport SDK application starts here.
+      sdk.Model.getData().then(function(modelData){
+        console.log('Model data loaded for sid:', modelData.sid);
+      });
+      sdk.on(sdk.Camera.Event.MOVE, function(pose){
+        console.log('Move called with', pose);
+      });
+    })
+    .catch(function(error) {
+      console.error(error);
     });
-    sdk.on(sdk.Camera.Event.MOVE, function(pose){
-      console.log('Move called with', pose);
-    });
-  })
-  .catch(function(error) {
-    console.error(error);
-  });
+  };
